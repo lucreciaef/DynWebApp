ada7edb2af004d8ba08f49d1d8ed20653c94cb91
lucreciaef <lucrecia.e.f@gmail.com>
2023-06-11 14:29:16 +0200

    adds basic paths + sql queries

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const express = require("express");
 const bodyParser= require ("body-parser");
 // Create Express instance
 const app = express();
-const port = 8084;
+
+const mysql = require("mysql2")
+const port = 8087;
+
+const db = mysql.createConnection ({
+	host: "localhost",
+	user: "root",
+	password: "testing1234!",
+	database: "myBookshop"
+});
+// connect to database
+db.connect((err) => {
+	if (err) {
+		throw err;
+	}
+	console.log("Connected to database");
+});
+global.db = db;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,31 +31,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Express app as an argument. You will add all the routes to this file later.
 require("./routes/main")(app);
 
-// Start the app
-
-// Old JS
-// http.createServer(app).listen(8083, function() {
-// 	console.log("Express app started");
-// });
-// ES6
-
-// http.createServer(app).listen(port, () => (
-// 	console.log("Express app started")));
-
 // This sets the path to the topic3/htmlExpress/views folder for the EJS engine
 app.set("views",__dirname + "/views");
 
-//  This tells Express that you want to use EJS as the templating engine for this application
+// This tells Express that you want to use EJS
+// as the templating engine for this application
 app.set("view engine", "ejs");
 
 // This tells Express that you want to render html pages
 app.engine("html", require("ejs").renderFile);
 
-// ES6
-app.get("/", (req, res
-) => res.send("<h1>Homepage</h1>"));
-
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+	console.log(`Example app listening on port ${port}!`));
--- routes/main.js
+++ routes/main.js
@@ -4,19 +4,38 @@
 module.exports = (app) => {
 	app.get("/", (req, res) => {
 		res.render("index.html", {
-			title:"Dynamic title",
-			heading1:"Dynamic heading"
+			title:"My Bookshop",
+			heading1:"Welcome to the new bookshop"
 		})
 	});
+
 	app.get("/search", (req, res) => {
 		res.render("search.html");
 	});
-	app.get("/search-result", (req, res) => {
+
+	app.get("/search-result-db", (req, res) => {
 		//searching in the database
-		// printing search keyword and results
-		res.send("This is the keyword you entered: " + req.query.keyword + "<br>" +
-			"This is the result of the search:");
+		let word = [req.query.keyword];
+		let sqlquery = "SELECT * FROM books WHERE name LIKE ? OR name LIKE ? OR name LIKE ?"; //back ` accent instead of quotes
+		// execute sql query
+
+		let q = db.format(sqlquery, [word, word+' %', '% '+word]);
+		console.log(q);
+
+		db.query(q,  (err, result) => {
+
+			if (err || result.length===0) {
+				// return console.error("No book found with the keyword you have entered"
+				// 	+ req.query.keyword + "error: "+ err.message);
+				res.redirect("./search");
+			}else{
+				// res.send(result);
+				//step4: (this will show the result of the search using an ejs template file, list.ejs can be used here)
+				res.render ('list.html',{availableBooks:result});
+			}
+		});
 	});
+
 	app.get("/about", (req, res) => {
 		res.render("about.html")
 	});
@@ -30,6 +49,35 @@ module.exports = (app) => {
 		// saving data in database
 		res.send("Hello "+ req.body.first + " "+ req.body.last +", you are now registered!" +
 		"We will send you a confirmation to your email address: " + req.body.email);
+	});
+
+	app.get("/list", function(req, res) {
+		// query database to get all the books
+		let sqlquery = "SELECT * FROM books";
+		// execute sql query
+		db.query(sqlquery, (err, result) => {
+			if (err) {
+				res.redirect("/");
+			}
+			res.render("list.html", {availableBooks: result});
+		});
+	});
+
+	app.get("/addbook", (req,res) => {
+		res.render("addbook.html");
+	});
+
+	app.post("/addbook", (req,res) => {
+		// saving data in database
+		let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+		// execute sql query
+		let newrecord = {name:req.body.name, price:req.body.price};
+		db.query(sqlquery, [newrecord.name, newrecord.price], (err, result) => {
+			if (err) {
+				return console.error(err.message);
+			}else
+				res.render("addedbook.html", {book:newrecord});
+		});
 	});
 }
 
